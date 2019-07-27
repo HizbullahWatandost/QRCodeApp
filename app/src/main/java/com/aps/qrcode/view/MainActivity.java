0d6e95aaa8f701ec2a1406d6880ab31719d5cc45
hizbullah <watandost10@gmail.com>
2019-07-27 16:56:52 +0430

    On new pc

--- app/src/main/java/com/aps/qrcode/view/MainActivity.java
+++ app/src/main/java/com/aps/qrcode/view/MainActivity.java
@@ -167,6 +167,7 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        // in new pc
         int id = item.getItemId();
         switch (id) {
             case R.id.secret_qr_create:
